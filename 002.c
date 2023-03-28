79b28d9caab0ad43d561fcbcf5cc2fa51ae6fea5
sanjan-15 <sanjand.btech22@rvu.edu.in>
2023-03-28 10:01:57 +0530

    exit

--- 002.c
+++ 002.c
@@ -1,10 +1,30 @@
 #include<stdio.h>
-
+ 
 int main()
 {
-    char a[20];
+    //int array
+    int n,i;
+    printf("Enter no. of elements of an array\n");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Your elements are:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    int s1 = sizeof(arr);
+    printf("Size of integer array is %d\n",s1);
+
+    //character array(string)
+    char a[10];
     printf("Enter character array\n");
-    scanf("%[^\n]s",a);
+    scanf("%s",&a);
     printf("Your character array is:\n");
     printf("%s",a);
+    int s2 = sizeof(a);
+    printf("Size of integer array is %d\n",s2);
 }
